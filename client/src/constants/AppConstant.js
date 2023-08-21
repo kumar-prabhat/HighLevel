a6f118163eb98a76f4c8d77ea358ede0cabaf52c
PD <prabhat.kumar@letsventure.com>
2023-08-21 11:30:06 +0530

    issue fixed

--- client/src/constants/AppConstant.js
+++ client/src/constants/AppConstant.js
@@ -1,7 +1,7 @@
 const BASE_URL = () => {
   return process.env.NODE_ENV === "production"
     ? "https://wallet-system-high-level-ca2da79447b7.herokuapp.com/api"
-    : "http:localhost:8000/api";
+    : "http://localhost:8000/api";
 };
 
 export { BASE_URL };
