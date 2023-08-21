f69ace28d1e4a39bdbf12099d7bc05f912b83660
PD <prabhat.kumar@letsventure.com>
2023-08-21 10:34:55 +0530

    fixed

--- config/index.js
+++ config/index.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 
 dotenv.config();
-const SERVER_PORT = process.env.SERVER_PORT || 80;
+const SERVER_PORT = process.env.SERVER_PORT || 8000;
 const MONGODB_URL = process.env.MONGODB_URL || "";
 
 const Config = {
